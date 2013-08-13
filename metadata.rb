4fef91f66ab5ceac9aabcc15d10196b46cfdd2f9
Joe Fitzgerald <joeyfitz@gmail.com>
2013-08-13 10:36:57 -0600

    Incrementing Version, Disabling Reboot

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'joe.fitzgerald@emc.com'
 license          'Apache 2.0'
 description      'Installs/Configures SQL CE 4.0 SP1'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 supports         "windows"
 depends          'windows'
